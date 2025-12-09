f9702664229ed351cde83269162cd287f7b23cfb
ShinYEB <dpqls9799@naver.com>
2025-12-09 18:02:32 +0900

    [FEAT] 캐싱 삭제

--- server/service/chatbotService.py
+++ server/service/chatbotService.py
@@ -1,15 +1,9 @@
 from component.ragchain import Ragchain
-from component.redis import search_cache, add_to_cache
 
 ragchain = Ragchain()
 
 def generate(query):
-    cached_answer = search_cache(query)
-
-    if cached_answer:
-        return cached_answer
 
     llm_answer = ragchain.generate(query)
-    add_to_cache(query, llm_answer)
 
     return llm_answer
